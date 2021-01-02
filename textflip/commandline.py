2214e134ab84c28ff90db35248a49ec1e6c1ee58
Tushar Sadhwani <tushar.sadhwani000@gmail.com>
2021-01-02 18:57:23 +0530

    Add stdin support

--- textflip/commandline.py
+++ textflip/commandline.py
@@ -1,9 +1,17 @@
 import argparse
+import sys
 
 from .rotation import flip
 
 
 def flip_cli():
+    if not sys.stdin.isatty():
+        lines = input().splitlines()
+        for line in lines:
+            print(flip(line))
+
+        return
+
     parser = argparse.ArgumentParser()
     parser.add_argument('text')
 
