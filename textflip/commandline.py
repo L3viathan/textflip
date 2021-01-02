cb42e77b192da0c20bf05e2f60c8f3aa94b40b51
Tushar Sadhwani <tushar.sadhwani000@gmail.com>
2021-01-02 22:12:43 +0530

    Refactor

--- textflip/commandline.py
+++ textflip/commandline.py
@@ -1,4 +1,3 @@
-import argparse
 import sys
 
 from .rotation import flip
@@ -6,14 +5,14 @@ from .rotation import flip
 
 def flip_cli():
     if not sys.stdin.isatty():
-        lines = input().splitlines()
+        lines = sys.stdin.read().splitlines()
         for line in lines:
             print(flip(line))
 
         return
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument('text')
+    if len(sys.argv) < 2:
+        sys.stderr.write("Error: no text provided\n")
+        sys.exit(1)
 
-    args = parser.parse_args()
-    print(flip(args.text))
+    print(flip(sys.argv[1]))
