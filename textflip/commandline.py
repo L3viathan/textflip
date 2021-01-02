81e69dfeb0da52cd14d4d07cb0f171e3ede5943c
Tushar Sadhwani <tushar.sadhwani000@gmail.com>
2021-01-02 22:37:08 +0530

    Improve stdin support

--- textflip/commandline.py
+++ textflip/commandline.py
@@ -4,15 +4,12 @@ from .rotation import flip
 
 
 def flip_cli():
-    if not sys.stdin.isatty():
-        lines = sys.stdin.read().splitlines()
-        for line in lines:
-            print(flip(line))
-
+    if len(sys.argv) > 1:
+        print(flip(sys.argv[1]))
         return
 
-    if len(sys.argv) < 2:
-        sys.stderr.write("Error: no text provided\n")
-        sys.exit(1)
-
-    print(flip(sys.argv[1]))
+    try:
+        for line in sys.stdin:
+            print(flip(line.rstrip()))
+    except KeyboardInterrupt:
+        print()
